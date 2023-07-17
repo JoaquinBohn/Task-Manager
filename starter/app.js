a9ee29268d07f5d08ccdab16bba729c2b43722a9
Joaquin Bohn <joaquinbohnv@gmail.com>
2023-07-17 14:29:17 -0300

    Conexion y crud con mongoDB

--- starter/app.js
+++ starter/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
-const PORT = 3000;
+// middleware
+app.use(express.json());
 
 // routes
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
 
-app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+app.use("/api/v1/tasks", tasks);
+
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
